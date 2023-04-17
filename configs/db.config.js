81e328babaaaba9e03af4bcc4b1698a8e3a923e2
anilkumarbora <71400179+anilkumarbora@users.noreply.github.com>
2023-04-17 23:42:23 +0530

    Update db.config.js

--- configs/db.config.js
+++ configs/db.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   DB_NAME: "find-my-restaurant-db",
-  DB_URL: "mongodb://localhost:27017",
+  DB_URL: "mongodb+srv://anil:4t8IozjVNLWTtFXn@cluster0.eqtdjja.mongodb.net/?retryWrites=true&w=majority",
 };
